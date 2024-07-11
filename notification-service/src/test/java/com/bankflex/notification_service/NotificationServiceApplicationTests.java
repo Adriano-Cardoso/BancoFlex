918ab63569ec337b4ff53b6af1d37615e4b62ac4
Adriano-Cardoso <goularta97@gmail.com>
2024-07-11 10:32:43 -0300

    feat: crate services

---
+++ notification-service/src/test/java/com/bankflex/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankflex.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
